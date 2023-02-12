d735674e8d1e981473cf1a03b2a46c3d0e615023
lllod <ygo.admin@gmail.com>
2023-02-12 17:52:37 +0400

    add seminar9 task1

---
+++ seminar9/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumElements(int m, int n)
+{
+    if (m <= n) { return m + SumElements(m + 1, n); }
+    return 0;
+}
+
+
+System.Console.WriteLine(SumElements(4, 8));
